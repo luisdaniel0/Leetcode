a458f6ab743af09529ec5bf64c96c71e00700374
Luis Sanchez <luisanchezdh@gmail.com>
2022-12-08 20:00:23 -0500

    answered

---
+++ codewars.js
@@ -0,0 +1,18 @@
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  var num = 0;
+  
+  for(var i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      num++;
+      
+  return num;
+}
+
+
+
+
+
+function booleanToString(b){
+  return b ? 'true' : 'false';
+}
