f4b1253f31f9d8694527cf3b32e4e04ab4ef0bef
Luis Sanchez <luisanchezdh@gmail.com>
2022-12-08 20:09:24 -0500

    answer

--- codewars.js
+++ codewars.js
@@ -1,18 +1,51 @@
-function countSheeps(arrayOfSheep) {
-  // TODO May the force be with you
-  var num = 0;
+// function countSheeps(arrayOfSheep) {
+//   // TODO May the force be with you
+//   var num = 0;
   
-  for(var i = 0; i < arrayOfSheep.length; i++)
-    if(arrayOfSheep[i] == true)
-      num++;
+//   for(var i = 0; i < arrayOfSheep.length; i++)
+//     if(arrayOfSheep[i] == true)
+//       num++;
       
-  return num;
-}
+//   return num;
+// }
 
 
 
 
 
-function booleanToString(b){
-  return b ? 'true' : 'false';
+// function booleanToString(b){
+//   return b ? 'true' : 'false';
+// }
+
+
+
+
+// function updateLight(current) {
+  
+//   if (current === 'green'){
+//     current = 'yellow'
+//   } else if (current === 'yellow') {
+//     current = 'red'
+//   } else if (current === 'red') {
+//     current = 'green'
+//   } else {}
+
+// return current
+
+
+// }
+
+
+
+function strCount(str, letter){  
+  
+  let count = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == letter)
+      count++;
+  }
+  
+  return count;
+  
 }
