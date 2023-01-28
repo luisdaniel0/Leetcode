3518ec3690adb8bda270f2391716134d5ab297da
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-28 17:58:08 -0500

    added leetcode question, 2sum again

--- codewars.js
+++ codewars.js
@@ -89,15 +89,3 @@ let twoSum = function (nums, target) {
     }
   }
 }
-
-let twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
-    }
-  }
-}
-console.log("123");
-console.log(1123);
