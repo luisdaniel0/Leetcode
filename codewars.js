9a5cbc60507e8f989c5af376c300cfbaf70b6880
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-31 17:56:03 -0500

    added leetcode question, 2sum again

--- codewars.js
+++ codewars.js
@@ -81,11 +81,22 @@ Only one valid answer exists.
 */
 
 let twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++){
-    for (let j = i + 1; j < nums.length; j++){
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        return [i,j]
+        return [i, j]
       }
     }
   }
 }
+
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+console.log("123");
